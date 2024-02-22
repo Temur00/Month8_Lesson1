ae391de95ff3cbc02a977f58878d73bb3d8a359d
Temur <shernazarovtemurcom001@gmail.com>
2024-02-22 18:35:27 +0500

    added

--- main.js
+++ main.js
@@ -1,80 +1,159 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//  TYPES
 // SELECT
-var getElementById = function (id) {
-    return document.getElementById(id);
-};
-var firstName = getElementById("firstName");
-var lastName = getElementById("lastName");
-var dateOfBirth = getElementById("dateOfBirth");
-var salary = getElementById("salary");
-var filterLevel = getElementById("filterLevel");
-var typeOfJob = getElementById("typeOfJob");
-var isMarried = getElementById("isMarried");
+var firstName = document.getElementById("firstName");
+var lastName = document.getElementById("lastName");
+var group = document.getElementById("group");
+var dateOfBirth = document.getElementById("dateOfBirth");
+var salary = document.getElementById("salary");
+var filterLevel = document.getElementById("filter");
+var typeOfJob = document.getElementById("typeOfJob");
+var isMarried = document.getElementById("isMarried");
 // EDIT
-var firstNameEdit = getElementById("firstNameEdit");
-var lastNameEdit = getElementById("lastNameEdit");
-var groupEdit = getElementById("groupEdit");
-var dateOfBirthEdit = getElementById("dateOfBirthEdit");
-var salaryEdit = getElementById("salaryEdit");
-var filterLevelEdit = getElementById("filterLevelEdit");
-var typeOfJobEdit = getElementById("typeOfJobEdit");
-var isMarriedEdit = getElementById("isMarriedEdit");
-var filterSelect = getElementById("filterSelect");
-var filterCountry = getElementById("filterCountry");
-// const studentsList = getElementById<HTMLInputElement>("studentsList");
-// const btnAddStudent = document.getElementById("btnAddStudent");
-// const btnAddStudent = document.getElementById("btnAddStudent") as HTMLInputElement;
-var btnEditStudent = (getElementById);
-var studentsList = document.getElementById("studentsList");
-var students = JSON.parse(localStorage.getItem("students") || "[]");
+var firstNameEdit = document.getElementById("firstNameEdit");
+var lastNameEdit = document.getElementById("lastNameEdit");
+var groupEdit = document.getElementById("groupEdit");
+var dateOfBirthEdit = document.getElementById("dateOfBirthEdit");
+var salaryEdit = document.getElementById("salaryEdit");
+var filterLevelEdit = document.getElementById("filterLevelEdit");
+var typeOfJobEdit = document.getElementById("typeOfJobEdit");
+var isMarriedEdit = document.getElementById("isMarriedEdit");
+var filterSelect = document.getElementById("filter-select");
+var filterCountry = document.getElementById("filter-country");
+var studentsList = document.getElementById("students-list");
+var btnAddStudent = document.getElementById("btn-add-student");
+var btnEditStudent = document.getElementById("btn-edit-student");
+var students = JSON.parse(localStorage.getItem("students")) || [];
+displayStudents(students);
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\
 // DISPLAY STUDENTS  ///////READ
 function displayStudents(students) {
     var str = "";
     students.map(function (student, i) {
-        str += "\n        <tr>\n                  <td>".concat(i + 1, "</td>\n                  <td>").concat(student.firstName, "</td>\n                  <td>").concat(student.lastName, "</td>\n                  <td>").concat(student.group, "</td>\n                  <td>").concat(student.dateOfBirth, "</td>\n                  <td>").concat(student.filterLevel, "</td>\n                  <td>").concat(student.typeOfJob, "</td>\n                  <td>").concat(student.salary, "</td>\n                  <td>").concat(student.isMarried ? "Yes" : "No", "</td>\n                  <td>\n                    <button class=\"btn bg-primary btn-sm text-light\" data-bs-toggle=\"modal\"\n                    data-bs-target=\"#editModal\" onclick=\"editStudent(").concat(student.id, ")\">Edit</button>\n                    <button class=\"btn btn-danger btn-sm\" onclick='deleteStudent(").concat(student.id, ")'>Delete</button>\n                  </td>\n        </tr>\n        ");
+        str += "\n      <tr>\n                <td>".concat(i + 1, "</td>\n                <td>").concat(student.firstName, "</td>\n                <td>").concat(student.lastName, "</td>\n                <td>").concat(student.group, "</td>\n                <td>").concat(student.dateOfBirth, "</td>\n                <td>").concat(student.filterLevel, "</td>\n                <td>").concat(student.typeOfJob, "</td>\n                <td>").concat(student.salary, "</td>\n                <td>").concat(student.isMarried ? "Yes" : "No", "</td>\n                <td>\n                  <button class=\"btn bg-primary btn-sm text-light\" data-bs-toggle=\"modal\"\n                  data-bs-target=\"#editModal\" onclick=\"editStudent(").concat(student.id, ")\">Edit</button>\n                  <button class=\"btn btn-danger btn-sm\" onclick='deleteStudent(").concat(student.id, ")'>Delete</button>\n                </td>\n      </tr>\n      ");
     });
     studentsList.innerHTML = str;
 }
 displayStudents(students);
-// /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
-// ADD STUDENT   //////
-// const btnAddStudent = document.getElementById("btnAddStudent");
-// const btnAddStudent = document.getElementById(
-//   "btnAddStudent"
-// ) as HTMLInputElement;
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+// ADD STUDENT   //////CREATE
 function addStudent() {
-    var firstNameInput = document.getElementById("firstName");
-    var lastNameInput = document.getElementById("lastName");
-    var groupSelect = document.getElementById("group");
-    var dateOfBirthInput = document.getElementById("dateOfBirth");
-    var filterLevelInput = document.getElementById("filterLevel");
-    var typeOfJobInput = document.getElementById("typeOfJob");
-    var salaryInput = document.getElementById("salary");
-    var isMarriedCheckbox = document.getElementById("isMarried");
     var student = {
         id: students.length + 1,
-        firstName: firstNameInput.value,
-        lastName: lastNameInput.value,
-        group: groupSelect.value,
-        dateOfBirth: dateOfBirthInput.value,
-        filterLevel: filterLevelInput.value,
-        typeOfJob: typeOfJobInput.value,
-        salary: salaryInput.value,
-        isMarried: isMarriedCheckbox.checked,
+        firstName: firstName.value,
+        lastName: lastName.value,
+        group: group.value,
+        dateOfBirth: dateOfBirth.value,
+        filterLevel: filterLevel.value,
+        typeOfJob: typeOfJob.value,
+        salary: salary.value,
+        isMarried: isMarried.checked,
     };
     students.push(student);
     localStorage.setItem("students", JSON.stringify(students));
     location.reload();
-    firstNameInput.value = "";
-    lastNameInput.value = "";
-    groupSelect.value = "";
-    dateOfBirthInput.value = "";
-    filterLevelInput.value = "";
-    typeOfJobInput.value = "";
-    salaryInput.value = "";
-    isMarriedCheckbox.checked = false;
+    firstName.value = "";
+    lastName.value = "";
+    group.value = "";
+    dateOfBirth.value = "";
+    filterLevel.value = "";
+    typeOfJob.value = "";
+    salary.value = "";
+    isMarried.checked = false;
 }
-var btnAddStudent = document.getElementById("btnAddStudent");
-btnAddStudent === null || btnAddStudent === void 0 ? void 0 : btnAddStudent.addEventListener("click", addStudent);
+btnAddStudent.addEventListener("click", addStudent);
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+// DELETE STUDENT  /////DELETE
+function deleteStudent(id) {
+    if (confirm("Are you sure to delete this student ?")) {
+        var newStudents = students.filter(function (std) { return std.id !== id; });
+        localStorage.setItem("students", JSON.stringify(newStudents));
+        location.reload();
+    }
+}
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+// EDIT STUDENTS ////EDIT
+var studentEditing;
+function editStudent(id) {
+    var student = students.find(function (student) { return student.id === id; });
+    if (student) {
+        studentEditing = student;
+        firstNameEdit.value = student.firstName;
+        lastNameEdit.value = student.lastName;
+        groupEdit.value = student.group;
+        dateOfBirthEdit.value = student.dateOfBirth;
+        salaryEdit.value = student.salary;
+        filterLevelEdit.value = student.filterLevel;
+        typeOfJobEdit.value = student.typeOfJob;
+        isMarriedEdit.checked = student.isMarried;
+    }
+}
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+// UPDATE STUDENTS ////
+function updateStudent() {
+    if (studentEditing) {
+        var student_1 = {
+            id: studentEditing.id,
+            firstName: firstNameEdit.value,
+            lastName: lastNameEdit.value,
+            group: groupEdit.value,
+            dateOfBirth: dateOfBirthEdit.value,
+            filterLevel: filterLevelEdit.value,
+            typeOfJob: typeOfJobEdit.value,
+            salary: salaryEdit.value,
+            isMarried: isMarriedEdit.checked,
+        };
+        var newStudents = students.map(function (st) {
+            return st.id === student_1.id ? student_1 : st;
+        });
+        localStorage.setItem("students", JSON.stringify(newStudents));
+        location.reload();
+    }
+}
+btnEditStudent.addEventListener("click", updateStudent);
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+// FILTER LEVEL
+filterSelect.addEventListener("change", function (e) {
+    var grpp = e.target.value;
+    var newStudentsList = [];
+    if (grpp === "Select a level") {
+        newStudentsList = students;
+    }
+    else {
+        newStudentsList = students.filter(function (st) { return st.filterLevel === grpp; });
+    }
+    displayStudents(newStudentsList);
+});
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+// FILTER COUNTRIES
+filterCountry.addEventListener("change", function (e) {
+    var grp = e.target.value;
+    var newStudentsList = [];
+    if (grp === "Select your country") {
+        newStudentsList = students;
+    }
+    else {
+        newStudentsList = students.filter(function (st) { return st.group === grp; });
+    }
+    displayStudents(newStudentsList);
+});
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+// SEARCH STUDENTS //////////////////
+search.addEventListener("input", function (e) {
+    var text = e.target.value;
+    var newSearchedList = [];
+    if (text === "") {
+        newSearchedList = students;
+    }
+    else {
+        newSearchedList = students.filter(function (st) {
+            return st.firstName.toLowerCase().includes(text.toLowerCase()) ||
+                st.lastName.toLowerCase().includes(text.toLowerCase()) ||
+                st.group.toLowerCase().includes(text.toLowerCase()) ||
+                st.group.toLowerCase().includes(text.toLowerCase()) ||
+                st.filterLevel.toLowerCase().includes(text.toLowerCase()) ||
+                st.salary.toLowerCase().includes(text.toLowerCase()) ||
+                st.dateOfBirth.toLowerCase().includes(text.toLowerCase()) ||
+                st.typeOfJob.toLowerCase().includes(text.toLowerCase());
+        });
+    }
+    displayStudents(newSearchedList);
+});
--- main.ts
+++ main.ts
@@ -1,141 +1,231 @@
-export {};
-
-//  TYPES
-
 // SELECT
-const getElementById = <T extends HTMLElement>(id: string): T | null | string =>
-  document.getElementById(id) as T | null | string;
-
-const firstName = getElementById<HTMLInputElement>("firstName");
-const lastName = getElementById<HTMLInputElement>("lastName");
-const dateOfBirth = getElementById<HTMLInputElement>("dateOfBirth");
-const salary = getElementById<HTMLInputElement>("salary");
-const filterLevel = getElementById<HTMLInputElement>("filterLevel");
-const typeOfJob = getElementById<HTMLInputElement>("typeOfJob");
-const isMarried = getElementById<HTMLInputElement>("isMarried");
+const firstName = document.getElementById("firstName") as HTMLInputElement;
+const lastName = document.getElementById("lastName") as HTMLInputElement;
+const group = document.getElementById("group") as HTMLSelectElement;
+const dateOfBirth = document.getElementById("dateOfBirth") as HTMLInputElement;
+const salary = document.getElementById("salary") as HTMLInputElement;
+const filterLevel = document.getElementById("filter") as HTMLSelectElement;
+const typeOfJob = document.getElementById("typeOfJob") as HTMLSelectElement;
+const isMarried = document.getElementById("isMarried") as HTMLInputElement;
 
 // EDIT
-const firstNameEdit = getElementById<HTMLInputElement>("firstNameEdit");
-const lastNameEdit = getElementById<HTMLInputElement>("lastNameEdit");
-const groupEdit = getElementById<HTMLInputElement>("groupEdit");
-const dateOfBirthEdit = getElementById<HTMLInputElement>("dateOfBirthEdit");
-const salaryEdit = getElementById<HTMLInputElement>("salaryEdit");
-const filterLevelEdit = getElementById<HTMLInputElement>("filterLevelEdit");
-const typeOfJobEdit = getElementById<HTMLInputElement>("typeOfJobEdit");
-const isMarriedEdit = getElementById<HTMLInputElement>("isMarriedEdit");
+const firstNameEdit = document.getElementById(
+  "firstNameEdit"
+) as HTMLInputElement;
+const lastNameEdit = document.getElementById(
+  "lastNameEdit"
+) as HTMLInputElement;
+const groupEdit = document.getElementById("groupEdit") as HTMLSelectElement;
+const dateOfBirthEdit = document.getElementById(
+  "dateOfBirthEdit"
+) as HTMLInputElement;
+const salaryEdit = document.getElementById("salaryEdit") as HTMLInputElement;
+const filterLevelEdit = document.getElementById(
+  "filterLevelEdit"
+) as HTMLSelectElement;
+const typeOfJobEdit = document.getElementById(
+  "typeOfJobEdit"
+) as HTMLSelectElement;
+const isMarriedEdit = document.getElementById(
+  "isMarriedEdit"
+) as HTMLInputElement;
 
-const filterSelect = getElementById<HTMLInputElement>("filterSelect");
-const filterCountry = getElementById<HTMLInputElement>("filterCountry");
-// const studentsList = getElementById<HTMLInputElement>("studentsList");
-// const btnAddStudent = document.getElementById("btnAddStudent");
-// const btnAddStudent = document.getElementById("btnAddStudent") as HTMLInputElement;
+const filterSelect = document.getElementById(
+  "filter-select"
+) as HTMLSelectElement;
+const filterCountry = document.getElementById(
+  "filter-country"
+) as HTMLSelectElement;
+const studentsList = document.getElementById("students-list") as HTMLElement;
+const btnAddStudent = document.getElementById(
+  "btn-add-student"
+) as HTMLButtonElement;
+const btnEditStudent = document.getElementById(
+  "btn-edit-student"
+) as HTMLButtonElement;
 
-const btnEditStudent = getElementById<HTMLInputElement>;
+let students: Student[] = JSON.parse(localStorage.getItem("students")) || [];
 
-const studentsList = document.getElementById("studentsList") as HTMLElement;
+displayStudents(students);
 
 interface Student {
-  id: number | string;
+  id: number;
   firstName: string;
   lastName: string;
   group: string;
   dateOfBirth: string;
-  salary: string;
   filterLevel: string;
   typeOfJob: string;
+  salary: string;
   isMarried: boolean;
 }
 
-const students: Student[] = JSON.parse(
-  localStorage.getItem("students") || "[]"
-);
-// DISPLAY STUDENTS  ///////READ
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\
 
+// DISPLAY STUDENTS  ///////READ
 function displayStudents(students: Student[]) {
   let str = "";
   students.map((student, i) => {
     str += `
-        <tr>
-                  <td>${i + 1}</td>
-                  <td>${student.firstName}</td>
-                  <td>${student.lastName}</td>
-                  <td>${student.group}</td>
-                  <td>${student.dateOfBirth}</td>
-                  <td>${student.filterLevel}</td>
-                  <td>${student.typeOfJob}</td>
-                  <td>${student.salary}</td>
-                  <td>${student.isMarried ? "Yes" : "No"}</td>
-                  <td>
-                    <button class="btn bg-primary btn-sm text-light" data-bs-toggle="modal"
-                    data-bs-target="#editModal" onclick="editStudent(${
-                      student.id
-                    })">Edit</button>
-                    <button class="btn btn-danger btn-sm" onclick='deleteStudent(${
-                      student.id
-                    })'>Delete</button>
-                  </td>
-        </tr>
-        `;
+      <tr>
+                <td>${i + 1}</td>
+                <td>${student.firstName}</td>
+                <td>${student.lastName}</td>
+                <td>${student.group}</td>
+                <td>${student.dateOfBirth}</td>
+                <td>${student.filterLevel}</td>
+                <td>${student.typeOfJob}</td>
+                <td>${student.salary}</td>
+                <td>${student.isMarried ? "Yes" : "No"}</td>
+                <td>
+                  <button class="btn bg-primary btn-sm text-light" data-bs-toggle="modal"
+                  data-bs-target="#editModal" onclick="editStudent(${
+                    student.id
+                  })">Edit</button>
+                  <button class="btn btn-danger btn-sm" onclick='deleteStudent(${
+                    student.id
+                  })'>Delete</button>
+                </td>
+      </tr>
+      `;
   });
   studentsList.innerHTML = str;
 }
-
 displayStudents(students);
 
-// /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\
 
-// ADD STUDENT   //////
-// const btnAddStudent = document.getElementById("btnAddStudent");
-
-// const btnAddStudent = document.getElementById(
-//   "btnAddStudent"
-// ) as HTMLInputElement;
-
+// ADD STUDENT   //////CREATE
 function addStudent() {
-  const firstNameInput = document.getElementById(
-    "firstName"
-  ) as HTMLInputElement;
-  const lastNameInput = document.getElementById("lastName") as HTMLInputElement;
-  const groupSelect = document.getElementById("group") as HTMLSelectElement;
-  const dateOfBirthInput = document.getElementById(
-    "dateOfBirth"
-  ) as HTMLInputElement;
-  const filterLevelInput = document.getElementById(
-    "filterLevel"
-  ) as HTMLInputElement;
-  const typeOfJobInput = document.getElementById(
-    "typeOfJob"
-  ) as HTMLInputElement;
-  const salaryInput = document.getElementById("salary") as HTMLInputElement;
-  const isMarriedCheckbox = document.getElementById(
-    "isMarried"
-  ) as HTMLInputElement;
-
-  const student = {
+  const student: Student = {
     id: students.length + 1,
-    firstName: firstNameInput.value,
-    lastName: lastNameInput.value,
-    group: groupSelect.value,
-    dateOfBirth: dateOfBirthInput.value,
-    filterLevel: filterLevelInput.value,
-    typeOfJob: typeOfJobInput.value,
-    salary: salaryInput.value,
-    isMarried: isMarriedCheckbox.checked,
+    firstName: firstName.value,
+    lastName: lastName.value,
+    group: group.value,
+    dateOfBirth: dateOfBirth.value,
+    filterLevel: filterLevel.value,
+    typeOfJob: typeOfJob.value,
+    salary: salary.value,
+    isMarried: isMarried.checked,
   };
-
   students.push(student);
   localStorage.setItem("students", JSON.stringify(students));
   location.reload();
 
-  firstNameInput.value = "";
-  lastNameInput.value = "";
-  groupSelect.value = "";
-  dateOfBirthInput.value = "";
-  filterLevelInput.value = "";
-  typeOfJobInput.value = "";
-  salaryInput.value = "";
-  isMarriedCheckbox.checked = false;
+  firstName.value = "";
+  lastName.value = "";
+  group.value = "";
+  dateOfBirth.value = "";
+  filterLevel.value = "";
+  typeOfJob.value = "";
+  salary.value = "";
+  isMarried.checked = false;
+}
+btnAddStudent.addEventListener("click", addStudent);
+
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+// DELETE STUDENT  /////DELETE
+
+function deleteStudent(id: number | string) {
+  if (confirm("Are you sure to delete this student ?")) {
+    let newStudents = students.filter((std) => std.id !== id);
+
+    localStorage.setItem("students", JSON.stringify(newStudents));
+    location.reload();
+  }
+}
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+
+// EDIT STUDENTS ////EDIT
+let studentEditing: Student | undefined;
+function editStudent(id: number | string) {
+  let student = students.find((student) => student.id === id);
+  if (student) {
+    studentEditing = student;
+
+    firstNameEdit.value = student.firstName;
+    lastNameEdit.value = student.lastName;
+    groupEdit.value = student.group;
+    dateOfBirthEdit.value = student.dateOfBirth;
+    salaryEdit.value = student.salary;
+    filterLevelEdit.value = student.filterLevel;
+    typeOfJobEdit.value = student.typeOfJob;
+    isMarriedEdit.checked = student.isMarried;
+  }
 }
 
-const btnAddStudent = document.getElementById("btnAddStudent");
-btnAddStudent?.addEventListener("click", addStudent);
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+
+// UPDATE STUDENTS ////
+
+function updateStudent() {
+  if (studentEditing) {
+    const student: Student = {
+      id: studentEditing.id,
+      firstName: firstNameEdit.value,
+      lastName: lastNameEdit.value,
+      group: groupEdit.value,
+      dateOfBirth: dateOfBirthEdit.value,
+      filterLevel: filterLevelEdit.value,
+      typeOfJob: typeOfJobEdit.value,
+      salary: salaryEdit.value,
+      isMarried: isMarriedEdit.checked,
+    };
+    let newStudents = students.map((st) =>
+      st.id === student.id ? student : st
+    );
+    localStorage.setItem("students", JSON.stringify(newStudents));
+    location.reload();
+  }
+}
+
+btnEditStudent.addEventListener("click", updateStudent);
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+
+// FILTER LEVEL
+filterSelect.addEventListener("change", function (e) {
+  let grpp = (e.target as HTMLSelectElement).value;
+  let newStudentsList = [];
+  if (grpp === "Select a level") {
+    newStudentsList = students;
+  } else {
+    newStudentsList = students.filter((st) => st.filterLevel === grpp);
+  }
+  displayStudents(newStudentsList);
+});
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+
+// FILTER COUNTRIES
+filterCountry.addEventListener("change", function (e) {
+  let grp = (e.target as HTMLSelectElement).value;
+  let newStudentsList = [];
+  if (grp === "Select your country") {
+    newStudentsList = students;
+  } else {
+    newStudentsList = students.filter((st) => st.group === grp);
+  }
+  displayStudents(newStudentsList);
+});
+// \/\/\/\/\/\/\/\/\/\/\/\/\/\/\
+
+// SEARCH STUDENTS //////////////////
+search.addEventListener("input", function (e) {
+  let text = (e.target as HTMLInputElement).value;
+  let newSearchedList = [];
+  if (text === "") {
+    newSearchedList = students;
+  } else {
+    newSearchedList = students.filter(
+      (st) =>
+        st.firstName.toLowerCase().includes(text.toLowerCase()) ||
+        st.lastName.toLowerCase().includes(text.toLowerCase()) ||
+        st.group.toLowerCase().includes(text.toLowerCase()) ||
+        st.group.toLowerCase().includes(text.toLowerCase()) ||
+        st.filterLevel.toLowerCase().includes(text.toLowerCase()) ||
+        st.salary.toLowerCase().includes(text.toLowerCase()) ||
+        st.dateOfBirth.toLowerCase().includes(text.toLowerCase()) ||
+        st.typeOfJob.toLowerCase().includes(text.toLowerCase())
+    );
+  }
+  displayStudents(newSearchedList);
+});
